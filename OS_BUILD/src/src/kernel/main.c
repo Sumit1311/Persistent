865aa3f8274dded743833a3bfd11f3349d72f5b9
Sumit Toshniwal <Sumit.Toshniwal@jivesoftware.com>
2016-09-12 15:16:22 +0530

    adding code for actual kernel

---
+++ OS_BUILD/src/src/kernel/main.c
@@ -0,0 +1,37 @@
+/*
+ ======================================
+ main.cpp
+ -kernel startup code
+ ======================================
+ */
+
+#include "include/kernel/debug_display.h"
+
+void _cdecl main ()
+  {
+
+    int i=0x12;
+
+    debug_clr_scr (0x18);
+
+    debug_goto_xy (4,4);
+    debug_set_color (0x17);
+    debug_printf ("+-----------------------------------------+\n");
+    debug_printf ("|    MOS 32 Bit C++ Kernel Executing!     |\n");
+    debug_printf ("+-----------------------------------------+\n\n");
+    debug_set_color (0x12);
+
+    debug_set_color (0x12);
+    debug_printf ("\ni as integer ........................");
+    debug_printf ("\ni in hex ............................");
+
+    debug_goto_xy (25,8);
+    debug_set_color (0x1F);
+    debug_printf ("\n[%i]",i);
+    debug_printf ("\n[0x%x]",i);
+
+    debug_goto_xy (4,16);
+    debug_set_color (0x1F);
+    debug_printf ("\n\nI am preparing to load... Hold on, please... :)");
+
+  }
