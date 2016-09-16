496641047b199f1307212ae3d09acd4e24f9369d
Sumit <sumittoshniwal92@gmail.com>
2016-09-16 22:21:24 +0530

    applying stash

--- OS_BUILD/src/utils/launchos.sh
+++ OS_BUILD/src/utils/launchos.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libXpm.so bochs 'boot:floppy' 'floppya: 1_44=./bin/c.img, status=inserted'
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libXpm.so bochsdbg 'boot:floppy' 'floppya: 1_44=./bin/c.img, status=inserted'
