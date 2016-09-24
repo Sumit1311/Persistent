f01515e336c76c91d295579c2f462340722f2a44
Sumit <sumittoshniwal92@gmail.com>
2016-09-24 20:58:08 +0530

    removed inline assemblhy code

--- OS_BUILD/src/src/kernel/entry.c
+++ OS_BUILD/src/src/kernel/entry.c
@@ -6,40 +6,32 @@
  ====================================================
  */
 
-extern void main ();
+extern void
+main();
+
+void
+clear();
+
 //extern void _cdecl InitializeConstructors();
-extern void Exit ();
+extern void
+Exit();
 
-void  kernel_entry ()
-  {
+void
+kernel_entry()
+{
 
-#ifdef ARCH_X86
-    _asm
-      {
-        cli			// clear interrupts--Do not enable them yet
-        mov ax, 10h// offset 0x10 in gdt for data selector, remember?
-        mov ds, ax
-        mov es, ax
-        mov fs, ax
-        mov gs, ax
-        mov ss, ax// Set up base stack
-        mov esp, 0x90000
-        mov ebp, esp// store current stack pointer
-        push ebp
-      }
-#endif
+  //init();
 
-    //! Execute global constructors
-    //InitializeConstructors();
+  //! Execute global constructors
+  //InitializeConstructors();
 
-    //!	Call kernel entry point
-    main ();
+  //!	Call kernel entry point
+  main();
 
-    //! Cleanup all dynamic dtors
-    //Exit ();
+  //! Cleanup all dynamic dtors
+  //Exit ();
 
-#ifdef ARCH_X86
-    _asm cli
-#endif
-    for (;;);
-  }
+  clear();
+  for (;;)
+    ;
+}
