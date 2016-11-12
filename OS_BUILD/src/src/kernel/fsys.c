f5c54bed5714ec6409c76887a81e6097f6d7cbd8
Sumit <sumittoshniwal92@gmail.com>
2016-11-12 16:31:06 +0530

    adding some changes

--- OS_BUILD/src/src/kernel/fsys.c
+++ OS_BUILD/src/src/kernel/fsys.c
@@ -48,92 +48,110 @@ PFILESYSTEM _FileSystems[DEVICE_MAX];
 /**
  *	Opens a file
  */
-FILE volOpenFile(const char* fname) {
+FILE
+volOpenFile(const char* fname)
+{
 
-	if (fname) {
+  if (fname)
+    {
 
-		//! default to device 'a'
-		unsigned char device = 'a';
+      //! default to device 'a'
+      unsigned char device = 'a';
 
-		//! filename
-		char* filename = (char*) fname;
+      //! filename
+      char* filename = (char*) fname;
 
-		//! in all cases, if fname[1]==':' then the first character must be device letter
-		//! FIXME: Using fname[2] do to BUG 2. Please see main.cpp for info
-		if (fname[2] == ':') {
+      //! in all cases, if fname[1]==':' then the first character must be device letter
+      //! FIXME: Using fname[2] do to BUG 2. Please see main.cpp for info
+      if (fname[2] == ':')
+        {
 
-			device = fname[0];
-			filename += 3; //strip it from pathname
-		}
+          device = fname[0];
+          filename += 3; //strip it from pathname
+        }
 
-		//! call filesystem
-		if (_FileSystems[device - 'a']) {
+      //! call filesystem
+      if (_FileSystems[device - 'a'])
+        {
 
-			//! set volume specific information and return file
-			FILE file = _FileSystems[device - 'a']->Open(filename);
-			file.deviceID = device;
-			return file;
-		}
-	}
+          //! set volume specific information and return file
+          FILE file = _FileSystems[device - 'a']->Open(filename);
+          file.deviceID = device;
+          return file;
+        }
+    }
 
-	//! return invalid file
-	FILE file;
-	file.flags = FS_INVALID;
-	return file;
+  //! return invalid file
+  FILE file;
+  file.flags = FS_INVALID;
+  return file;
 }
 
 /**
  *	Reads file
  */
-void volReadFile(PFILE file, unsigned char* Buffer, unsigned int Length) {
+void
+volReadFile(PFILE file, unsigned char* Buffer, unsigned int Length)
+{
 
-	if (file)
-		if (_FileSystems[file->deviceID - 'a'])
-			_FileSystems[file->deviceID - 'a']->Read(file, Buffer, Length);
+  if (file)
+    if (_FileSystems[file->deviceID - 'a'])
+      _FileSystems[file->deviceID - 'a']->Read(file, Buffer, Length);
 }
 
 /**
  *	Close file
  */
-void volCloseFile(PFILE file) {
+void
+volCloseFile(PFILE file)
+{
 
-	if (file)
-		if (_FileSystems[file->deviceID - 'a'])
-			_FileSystems[file->deviceID - 'a']->Close(file);
+  if (file)
+    if (_FileSystems[file->deviceID - 'a'])
+      _FileSystems[file->deviceID - 'a']->Close(file);
 }
 
 /**
  *	Registers a filesystem
  */
-void volRegisterFileSystem(PFILESYSTEM fsys, unsigned int deviceID) {
+void
+volRegisterFileSystem(PFILESYSTEM fsys, unsigned int deviceID)
+{
 
-	static int i = 0;
+  static int i = 0;
 
-	if (i < DEVICE_MAX)
-		if (fsys) {
+  /*if (i < DEVICE_MAX)
+    {*/
+      if (fsys)
+        {
 
-			_FileSystems[deviceID] = fsys;
-			i++;
-		}
+          _FileSystems[deviceID] = fsys;
+          i++;
+        }
+  /*}*/
 }
 
 /**
  *	Unregister file system
  */
-void volUnregisterFileSystem(PFILESYSTEM fsys) {
+void
+volUnregisterFileSystem(PFILESYSTEM fsys)
+{
 
-	for (int i = 0; i < DEVICE_MAX; i++)
-		if (_FileSystems[i] == fsys)
-			_FileSystems[i] = 0;
+  for (int i = 0; i < DEVICE_MAX; i++)
+    if (_FileSystems[i] == fsys)
+      _FileSystems[i] = 0;
 }
 
 /**
  *	Unregister file system
  */
-void volUnregisterFileSystemByID(unsigned int deviceID) {
+void
+volUnregisterFileSystemByID(unsigned int deviceID)
+{
 
-	if (deviceID < DEVICE_MAX)
-		_FileSystems[deviceID] = 0;
+  if (deviceID < DEVICE_MAX)
+    _FileSystems[deviceID] = 0;
 }
 
 //============================================================================
