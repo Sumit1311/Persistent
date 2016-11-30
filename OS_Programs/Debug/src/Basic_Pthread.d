140bfc8f59787c14e0481b1b9b7d52d6f780d934
Sumit <sumittoshniwal92@gmail.com>
2016-11-30 15:32:20 +0530

    removing old files

--- OS_Programs/Debug/src/Basic_Pthread.d
+++
@@ -1,4 +0,0 @@
-src/Basic_Pthread.d: ../src/Basic_Pthread.c \
- /home/sumit/JavaWorkplace/OS_Programs/src/include/condition_lock.h
-
-/home/sumit/JavaWorkplace/OS_Programs/src/include/condition_lock.h:
--- OS_Programs/Debug/src/Conditional_Locks.d
+++
@@ -1,4 +0,0 @@
-src/Conditional_Locks.d: ../src/Conditional_Locks.c \
- /home/sumit/JavaWorkplace/OS_Programs/src/include/condition_lock.h
-
-/home/sumit/JavaWorkplace/OS_Programs/src/include/condition_lock.h:
